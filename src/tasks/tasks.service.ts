1fd67cea66e907eabb64a75d15e7fed0b7bf98e4
pelevin-d-y <natpuot1992@gmail.com>
2020-03-11 19:41:59 +0300

    add delete task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.module';
+import * as uuid from 'uuid/v1';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTAsks(): Task[] {
+    return this.tasks;
+  }
+
+  getTaskById(id: string): Task {
+    return this.tasks.find(task => task.id === id);
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+    return task;
+  }
+
+  deleteTask(id: string): string {
+    this.tasks = this.tasks.filter(item => {
+      console.log(item.id !== id);
+      return item.id !== id;
+    });
+    console.log('this.tasks', this.tasks);
+    return id;
+  }
+}
